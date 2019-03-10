ec816bc78027e0bb825d65a3af9a40bddf0ff08f
slawomir <sgicala98@gmail.com>
2019-03-10 23:11:43 +0100

    Add gitignore

--- lab1/zad1/library.c
+++ lab1/zad1/library.c
@@ -2,6 +2,60 @@
 
 #include <stdio.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+struct search_wrapper* init(int size){
+
+    if (size <= 0){
+        fprintf(stderr,"Array size has to be bigger than 0");
+        exit(-1);
+    }
+
+    struct search_wrapper* sw = calloc(1, sizeof(struct search_wrapper));
+    char** results = calloc(size, sizeof(char*));
+
+    if (sw == NULL || array == NULL){
+        fprintf(stderr, "Could not allocate memory");
+        exit(-1);
+    }
+
+    sw->size = size;
+    sw->search_results = results;
+
+    return sw;
+}
+
+void set_search_rules(struct search_wrapper* sw, char* directory, char* file, char* tmp_file){
+
+    sw->directory = directory;
+    sw->file = file;
+    sw->tmp_file = tmp_file;
+
+}
+
+int search(struct search_wrapper* sw){
+
+    if (sw == NULL || sw->search_results == NULL || sw->directory == NULL || sw->file == NULL || sw->tmp_file == NULL){
+        fprintf(stderr, "Correct data is not specified");
+        exit(-1);
+    }
+
+    char* command = calloc(1,strlen(bt->dir) + strlen(bt->file) + strlen(bt->temp) + 25);
+    sprintf(command, "find \"%s\" -iname \"%s\" > \"%s\"", sw->directory, sw->file, sw->tmp_file);
+
+    system(command);
+
+    free(command);
+
+    int i = 0;
+    while (i < sw->size && sw->search_results[i] != NULL){
+        i++;
+    }
+
+    if (sw->search_results[i] != NULL){
+        fprintf(stderr,"Array is full");
+        exit(-1);
+    }
+
+    //zmierz dlugosc pliku
+    //zaalokuj pamiec
+    //[rzepisz rzeczy z plku do zaalokowanej pamieci
 }
--- lab1/zad1/library.h
+++ lab1/zad1/library.h
@@ -8,15 +8,16 @@ typedef struct search_wrapper{
     char** search_results;
     char* directory;
     char* file;
+    char* tmp_file;
 };
 
 struct search_wrapper* init(int size);
 
-void set_search_rules(struct search_wrapper sw, char* directory, char* file);
+void set_search_rules(struct search_wrapper* sw, char* directory, char* file, char* tmp_file);
 
-int search(struct search_wrapper sw);
+int search(struct search_wrapper* sw);
 
-void remove_block(struct search_wrapper sw, int index);
+void remove_block(struct search_wrapper* sw, int index);
 
 
 #endif
